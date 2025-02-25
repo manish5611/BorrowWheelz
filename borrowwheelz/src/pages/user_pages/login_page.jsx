678f4818b62b967d062c06e68913cdc421389ae8
manish5611 <mamnishyogakirtibn20@gmail.com>
2025-02-25 16:58:22 +0530

    Login Page is Done

---
+++ borrowwheelz/src/pages/user_pages/login_page.jsx
@@ -0,0 +1,71 @@
+export default function Login() {
+    return (
+      <>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-primary">
+              Sign in
+            </h2>
+          </div>
+  
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form action="#" method="POST" className="space-y-6">
+              <div>
+                <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    required
+                    autoComplete="email"
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 border-1 border-orange-500 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                    Password
+                  </label>
+                  <a href="#" className="text-sm font-semibold text-orange-600 hover:text-indigo-500">
+                    Forgot password?
+                  </a>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                    autoComplete="current-password"
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 border-1 border-orange-500 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-orange-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Sign In
+                </button>
+              </div>
+            </form>
+  
+            <p className="mt-10 text-center text-sm/6 text-gray-500">
+              Don't have an account?{' '}
+              <a href="/register" className="font-semibold text-orange-600 hover:text-indigo-500">
+                Sign Up
+              </a>
+            </p>
+          </div>
+        </div>
+      </>
+    );
+  }
+  
